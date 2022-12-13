fd7605d0abf394e8e61bc72e1e215260ad79c58a
kjvlbu <114408460+kjvlbu@users.noreply.github.com>
2022-12-13 02:55:34 +0000

    Update Task2WIP.py

--- Task2WIP.py
+++ Task2WIP.py
@@ -22,13 +22,17 @@ if __name__ == '__main__':
         data = input("> ")
         runner_info = data.split("::")
         if not data == "END":
-            runner_list.append(runner_info[0])
-            time_list.append(runner_info[1])
+            runner_list.append(int(runner_info[0]))
+            time_list.append(int(runner_info[1]))
         elif data == "END" and len(runner_list) == 0 and len(time_list) == 0:
             print("Error. No data found.")
             break
+        ### elif runner_info[0] == "" and runner_info[1] == "":
+            ### print("Error in the data stream. Please continue.")
         elif data == "END" and not len(runner_list) == 0 and not len(time_list) == 0:
-            print("Total Runners: ", len(runner_list))
-            print("Average Time: ", mean(time_list))
-            print("Fastest Time: ", convert(min(time_list)))
-            print("Slowest Time:", convert(max(time_list)))
+            print("Total Runners:",len(runner_list))
+            print("Average Time:",convert(mean(time_list)))
+            print("Fastest Time:",convert(min(time_list)))
+            print("Slowest Time:",convert(max(time_list)))
+            print("Best Time Here: Runner #",runner_list[time_list.index(min(time_list))])
+            break
